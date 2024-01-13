a55fd914d53ebed86f56185f308bf00255f60f12
skcore <skcore1013@gmail.com>
2024-01-13 19:05:19 +0530

    Blazor life cycle events and working with js

---
+++ BlazorWebSimpleEvents/wwwroot/JS/test.js
@@ -0,0 +1,16 @@
+﻿function JSAlert() {
+    alert("Hello");
+}
+
+function AddTableRow(elem, firstname, lastname) {
+    let row = document.createElement("tr");
+
+    let cell1 = document.createElement("td");
+    let cell2 = document.createElement("td");
+    cell1.innerText = firstname;
+    cell2.innerText = lastname;
+
+    row.append(cell1);
+    row.append(cell2);
+    elem.append(row);
+} 
